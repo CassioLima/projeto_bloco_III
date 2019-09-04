b2765e689ba6d3768157932ce644e8fb1c3bd330
cassilima <lima164@gmail.com>
2019-09-04 14:26:50 -0300

    Projet 2o

---
+++ EventsListMicroservices.Domain/Model/Eventos.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventsListMicroservices.Domain.Model
+{
+    class Eventos
+    {
+    }
+}
---
+++ EventsListMicroservices.Infra.DataAccess/Repositories/Contexts/EventsListContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventsListMicroservices.Infra.DataAccess.Repositories.Contexts
+{
+    class EventsListContext
+    {
+    }
+}
